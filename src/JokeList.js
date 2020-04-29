2953a0532d60220505eba9043719baed6d9bef1f
Dora Belme <contact.dorabelme@gmail.com>
2020-04-29 15:14:24 -0400

    jokelist component

--- src/JokeList.js
+++ src/JokeList.js
@@ -5,10 +5,41 @@ import uuid from "uuid/v4";
 import "./JokeList.css";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount() {
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push(res.data.joke);
+    };
+
+    this.setState({ jokes: jokes });
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Dad Jokes</h1>
+      <div className="JokeList">
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
+
+        <div className="JokeList-jokes">
+          {this.state.jokes.map(j => (
+            <div>{j}</div>
+          ))}
+        </div>
       </div>
     )
   }
