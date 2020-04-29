708bf50a536d6b2ba2263ab4f2bd5a5eb93423b4
Dora Belme <contact.dorabelme@gmail.com>
2020-04-29 15:52:45 -0400

    joke component styling

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,9 +11,13 @@ class JokeList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes")) || "[]" };
   }
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com/", {
@@ -23,6 +27,7 @@ class JokeList extends Component {
     };
 
     this.setState({ jokes: jokes });
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
 
   handleVote(id, delta) {
