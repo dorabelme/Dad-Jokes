95e9d06519ad10bd4c433e34542980a3d9bf59d8
Dora Belme <contact.dorabelme@gmail.com>
2020-04-29 15:28:55 -0400

    voting logic

--- src/Joke.js
+++ src/Joke.js
@@ -5,7 +5,15 @@ class Joke extends Component {
 
   render() {
     return (
-      <div></div>
+      <div className="Joke">
+        <div className="Joke-buttons">
+          <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
+          <span>{this.props.votes}</span>
+          <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
+        </div>
+        <div className="Joke-text">{this.props.text}</div>
+
+      </div>
 
     );
   }
